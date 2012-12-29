14b7a66723320837ec3f02c0e971fe3aecee6409
igarmatenko <cheef.che@gmail.com>
2012-12-29 22:31:24 +0400

    Catching all user output to prevent spinner failure

--- lib/shell-spinner.rb
+++ lib/shell-spinner.rb
@@ -2,58 +2,80 @@ require "shell-spinner/version"
 
 module ShellSpinner
 
-  def self.wrap text = nil, &block
-    with_message text do
-      join_spinner_thread(block)
+  class Runner
+    def initialize
+      require 'stringio'
+      @buffer, @original_output = StringIO.new, $stdout
     end
-  end
 
-  private
+    def wrap_block text = nil, &block
+      with_message text do
+        with_spinner &block
+      end
+    end
 
-    def self.with_message text = nil
-      require 'colorize'
+    private
 
-      begin
-        print "#{text}... " unless text.nil?
+      def catch_user_output
+        $stdout = @buffer
         yield
-        print "done\n".colorize(:green) unless text.nil?
-      rescue Exception => e
-        print "fail\n".colorize(:red) unless text.nil?
-        re_raise_exception e
-      end
-    end
-
-    def self.join_spinner_thread proc
-      chars  = %w{ | / - \\ }
-      thread = Thread.new { proc.call }
-
-      while thread.alive?
-        print chars[0]
-        sleep 0.1
-        print "\b"
-
-        chars.push chars.shift
+      ensure
+        $stdout = @original_output
       end
 
-      thread.join
-    end
+      def with_message text = nil
+        require 'colorize'
 
-    def self.re_raise_exception e
-      raise begin
-        new_exception = build_new_exception(e)
-        new_exception.set_backtrace e.backtrace
-        new_exception
+        begin
+          print "#{text}... " unless text.nil?
+          catch_user_output { yield }
+          print "done\n".colorize(:green) unless text.nil?
+          print user_output.colorize(:blue)
+        rescue Exception => e
+          print "fail\n".colorize(:red) unless text.nil?
+          print user_output.colorize(:blue)
+          re_raise_exception e
+        end
       end
-    end
 
-    # Needs for cases when custom exceptions needs a several required arguments
-    def self.build_new_exception e
-      e.class.new(e.message)
-    rescue
-      Exception.new e.message
+      def user_output
+        @buffer.rewind
+        @buffer.read
+      end
+
+      def with_spinner
+        chars  = %w{ | / - \\ }
+        thread = Thread.new { yield }
+
+        while thread.alive?
+          @original_output.print chars[0]
+          sleep 0.1
+          @original_output.print "\b"
+
+          chars.push chars.shift
+        end
+
+        thread.join
+      end
+
+      def re_raise_exception e
+        raise begin
+          new_exception = build_new_exception(e)
+          new_exception.set_backtrace e.backtrace
+          new_exception
+        end
+      end
+
+      # Needs for cases when custom exceptions needs a several required arguments
+      def build_new_exception e
+        e.class.new(e.message)
+      rescue
+        Exception.new e.message
+      end
     end
 end
 
 def ShellSpinner text = nil, &block
-  ShellSpinner.wrap text, &block
+  runner = ShellSpinner::Runner.new
+  runner.wrap_block text, &block
 end
--- shell-spinner.gemspec
+++ shell-spinner.gemspec
@@ -3,15 +3,15 @@ $:.push File.expand_path("../lib", __FILE__)
 require "shell-spinner/version"
 
 Gem::Specification.new do |s|
-  s.name        = "shell-spinner"
-  s.version     = ShellSpinner::VERSION
-  s.authors     = ["Ivan Garmatenko"]
-  s.email       = %w(cheef.che@gmail.ru)
-  s.homepage    = "https://github.com/cheef/shell-spinner"
-  s.summary     = %q{Animated spinner for shell}
-  s.description = %q{Gem provides animated spinner for UNIX shell and could be used with rake tasks and any console scripts}
+  s.name          = "shell-spinner"
+  s.version       = ShellSpinner::VERSION
+  s.authors       = ["Ivan Garmatenko"]
+  s.email         = %w(cheef.che@gmail.ru)
+  s.homepage      = "https://github.com/cheef/shell-spinner"
+  s.summary       = %q{Animated spinner for shell}
+  s.description   = %q{Gem provides animated spinner for UNIX shell and could be used with rake tasks and any console scripts}
 
-  s.files         = `git ls-files`.split("\n")
+  s.files         = Dir['README.md', 'lib/**/*']
   s.require_paths = %w(lib)
 
   s.add_runtime_dependency "colorize"
